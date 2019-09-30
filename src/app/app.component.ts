cfbce53ab94249a2ecbbd7f3831928bd83d268d6
LBoisseron <kalaloolb@gmail.com>
2019-09-30 15:32:39 -0400

    w du 30/09/19
    -Découverte ANGULAR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
+/**
+ * pour déclarer une classe comme composant de notre application, on importe "component" via @angular/core
+ */
 import { Component } from '@angular/core';
 
+/**
+ * @component est ce qu'on appelle un décorateur.
+ * il va nous permettre de définir 3 paramètres essentiels à notre application...
+ */
 @Component({
+  /**
+   * le selector détermine le nom de la balise html permettant d'afficher notre composant dans l'application.
+   */
   selector: 'app-root',
+  /**
+   * le "templateUrl" ou "template" est la partie visible de notre composant, ce qui s'affiche à l'encran lorsque le composant est utlisé
+   */
   templateUrl: './app.component.html',
+  /**
+   * le "styleUrl" permet juste de déclarer les styles. NB: c'est un tableau
+   */
   styleUrls: ['./app.component.scss']
 })
+/**
+ * la class contient les données du composant, mais aussi son comportement
+ * dans notre contexte MVVM, notre class correspond au Model.
+ */
 export class AppComponent {
-  title = 'contact-app';
+  // -- déclaration d'une variable
+  title = 'Gestion de Contacts';
+
+  //déclaration d'un objet contact
+  unContact =
+    {
+      id: 1,
+      name: 'Léna BOISSERON',
+      username: 'lénaboisseron',
+      email: 'lboisseron@yahoo.fr'
+      };
+
+  //tableau de contacts
+  mesContacts = [
+    {
+      id: 2,
+      name: 'Laureen LABUTHIE',
+      username: 'laureenlabuthie',
+      email: 'laureen@yahoo.fr'
+    },
+    {
+      id: 3,
+      name: 'Astrid JONATHAN',
+      username: 'astridjonathan',
+      email: 'astrid@yahoo.fr'
+    },
+    {
+      id: 3,
+      name: 'Rosemael ANTONY',
+      username: 'rosemaelantony',
+      email: 'rosemael@yahoo.fr'
+    },
+  ];
 }
